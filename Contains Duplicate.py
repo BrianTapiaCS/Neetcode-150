19629833eba145161f777b5b5d6a24c726fbbbdb
BrianTapiaCS <brian@gmail/com>
2025-07-15 17:46:09 -0400

    Leetcode 217 (Easy) Solved

---
+++ Contains Duplicate.py
@@ -0,0 +1,33 @@
+'''
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+'''
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n, copy = len(nums), len(set(nums))
+        return n != copy
